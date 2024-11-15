3a9d1dc97b857924585a5657650308ef72a2f5fd
Nikson <nikolaking88@gmail.com>
2024-11-15 22:55:51 +0100

    Added basic routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive, Router, NavigationEnd } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { SideMenuComponent } from './components/side-menu/side-menu.component';
 import { RegisterOdFormComponent } from './components/register-od-form/register-od-form.component';
@@ -9,18 +9,34 @@ import { EventsComponent } from "./components/event/events/events.component";
 import { MerchandiseComponent } from "./components/merchandise/merchandise/merchandise.component";
 import { BreadcrumbComponent } from "./components/breadcrumb/breadcrumb/breadcrumb.component";
 import { PrimeNGConfig } from 'primeng/api';
+import { Subject, takeUntil, tap } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, HeaderComponent, SideMenuComponent, RegisterOdFormComponent, RegisterPupFormComponent, LoginFormComponent, EventsComponent, MerchandiseComponent, BreadcrumbComponent],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, HeaderComponent, SideMenuComponent, RegisterOdFormComponent, RegisterPupFormComponent, LoginFormComponent, EventsComponent, MerchandiseComponent, BreadcrumbComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
-  constructor(private primengConfig: PrimeNGConfig){}
+  showBasicNavigation = true;
+  private readonly destroy$ = new Subject<void>();
+  
+  constructor(private primengConfig: PrimeNGConfig, private router: Router){}
+
   ngOnInit(): void {
     this.primengConfig.ripple = true;
+    this.router.events.pipe(takeUntil(this.destroy$),tap((event) =>{
+      if (event instanceof NavigationEnd) {
+        // List of routes that should not show the navbar
+        const excludedRoutes = ['/', '/register-od', '/register-pup'];
+
+        // Check if the current route is in the list of excluded routes
+        this.showBasicNavigation = !excludedRoutes.includes(event.urlAfterRedirects);
+      }
+    })).subscribe();
   }
+
   title = 'EventPlanner';
 }
