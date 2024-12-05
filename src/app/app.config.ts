9de13a956f54066e6a2ade4f52083b28523fb22f
Nikson <nikolaking88@gmail.com>
2024-12-05 18:02:07 +0100

    Added Interceptor, AuthGuard and JwtService for authentication

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,8 +4,9 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { Interceptor } from './components/auth/http-interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(),provideAnimations(),provideHttpClient(withFetch())]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(),provideAnimations(),provideHttpClient(withFetch()), { provide: HTTP_INTERCEPTORS, useClass: Interceptor, multi: true }]
 };
