be955b0dd5e2ef080f966b89be3b3c7c0abcdc00
MihajloCuic <158320399+MihajloCuic@users.noreply.github.com>
2025-01-18 18:42:37 +0100

    fixed merge conflict mistakes

--- src/app/merchandise/service/service-details/service-details.component.ts
+++ src/app/merchandise/service/service-details/service-details.component.ts
@@ -8,7 +8,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { MerchandiseService } from '../../merchandise/merchandise.service';
 import { CurrencyPipe } from '@angular/common';
 import { CommonModule } from '@angular/common';
-import { GalleriaModule} from 'primeng/galleria';
+import { GalleriaModule } from 'primeng/galleria';
 import { FieldsetModule } from 'primeng/fieldset';
 import { PaginatorModule } from 'primeng/paginator';
 import { HttpErrorResponse } from '@angular/common/http';
@@ -16,18 +16,29 @@ import { MapComponent } from '../../../shared/map/map.component';
 import { MapService } from '../../../shared/map/map.service';
 import { JwtService } from '../../../infrastructure/auth/jwt.service';
 import { catchError, EMPTY, switchMap, tap } from 'rxjs';
-import { LeaveReviewComponent } from "../../../review/leave-review/leave-review.component";
+import { LeaveReviewComponent } from '../../../review/leave-review/leave-review.component';
 import { ReviewService } from '../../../review/review-service.service';
 import { ReviewType } from '../../../review/leave-review/review-request-dto';
 import { PhotoService } from '../../../shared/photos/photo.service';
 
-
 @Component({
   selector: 'app-service-details',
   standalone: true,
-  imports: [PaginatorModule, MapComponent, ButtonModule, ReservationDialogComponent, FloatLabelModule, InputTextModule, CurrencyPipe, CommonModule, GalleriaModule, FieldsetModule, LeaveReviewComponent],
+  imports: [
+    PaginatorModule,
+    MapComponent,
+    ButtonModule,
+    ReservationDialogComponent,
+    FloatLabelModule,
+    InputTextModule,
+    CurrencyPipe,
+    CommonModule,
+    GalleriaModule,
+    FieldsetModule,
+    LeaveReviewComponent,
+  ],
   templateUrl: './service-details.component.html',
-  styleUrl: './service-details.component.scss'
+  styleUrl: './service-details.component.scss',
 })
 export class ServiceDetailsComponent implements OnInit {
   @ViewChild(ReservationDialogComponent)
@@ -41,95 +52,108 @@ export class ServiceDetailsComponent implements OnInit {
   isVisible: boolean = false;
   responsiveOptions: any[] = [
     {
-        breakpoint: '991px',
-        numVisible: 4
+      breakpoint: '991px',
+      numVisible: 4,
     },
     {
-        breakpoint: '767px',
-        numVisible: 3
+      breakpoint: '767px',
+      numVisible: 3,
     },
     {
-        breakpoint: '575px',
-        numVisible: 1
-    }
-];
+      breakpoint: '575px',
+      numVisible: 1,
+    },
+  ];
 
-  constructor(private photoService: PhotoService,
-              private route: ActivatedRoute, 
-              private merchandiseService: MerchandiseService,
-              private mapService:MapService, 
-              public jwtService: JwtService, 
-              private router: Router,
-              private reviewService: ReviewService) {}
+  constructor(
+    private photoService: PhotoService,
+    private route: ActivatedRoute,
+    private merchandiseService: MerchandiseService,
+    private mapService: MapService,
+    public jwtService: JwtService,
+    private router: Router,
+    private reviewService: ReviewService
+  ) {}
 
   ngOnInit() {
     const id = this.route.snapshot.paramMap.get('id');
     this.serviceId = id ? Number(id) : -1;
 
-    if(this.serviceId != -1) {
+    if (this.serviceId != -1) {
       this.merchandiseService
-  .getMerchandiseDetails(this.serviceId)
-  .pipe(
-    switchMap((response) => {
-      this.service = response;
-      this.images = this.service.merchandisePhotos.map(x => this.photoService.getPhotoUrl(x.photo));
-      this.paginatedReviews = this.service?.reviews.slice(0, 5);
-      this.mapService.updateMerchandiseAddresses(response);
+        .getMerchandiseDetails(this.serviceId)
+        .pipe(
+          switchMap((response) => {
+            this.service = response;
+            this.images = this.service.merchandisePhotos.map((x) =>
+              this.photoService.getPhotoUrl(x.photo)
+            );
+            this.paginatedReviews = this.service?.reviews.slice(0, 5);
+            this.mapService.updateMerchandiseAddresses(response);
 
-      // Call to check if the service is favorited
-      return this.merchandiseService.getFavorites().pipe(
-        tap((favorites) => {
-          this.isFavorited = favorites.some((x) => x.id === this.service?.id);
-        })
-      )
-      .subscribe();
+            // Call to check if the service is favorited
+            return this.merchandiseService.getFavorites().pipe(
+              tap((favorites) => {
+                this.isFavorited = favorites.some(
+                  (x) => x.id === this.service?.id
+                );
+              })
+            );
+          })
+        )
+        .subscribe();
 
-      this.reviewService.isEligibleForReview(this.jwtService.getIdFromToken(), this.serviceId, ReviewType.MERCHANIDSE_REVIEW).subscribe({
-        next: (response) => {
-          this.isVisible = response;
-        },
-        error: (err) => {
-          this.isVisible = false;
-        }
-      });
+      this.reviewService
+        .isEligibleForReview(
+          this.jwtService.getIdFromToken(),
+          this.serviceId,
+          ReviewType.MERCHANIDSE_REVIEW
+        )
+        .subscribe({
+          next: (response) => {
+            this.isVisible = response;
+          },
+          error: (err) => {
+            this.isVisible = false;
+          },
+        });
     }
   }
   private getErrorMessage(error: HttpErrorResponse): string {
-      // Check if error response has a body with a message
-      if (error.error instanceof ErrorEvent) {
-        // Client-side error
-        return error.error.message || 'Client-side error occurred';
-      } else {
-        // Server-side error
-        // Try multiple ways to extract the error message
-        if (error.error && error.error.message) {
-          // Directly from error object
-          return error.error.message;
-        }
-
-        if (error.error && typeof error.error === 'string') {
-          // If error is a string message
-          return error.error;
-        }
-
-        if (error.error && error.error.errorMessage) {
-          // Alternative error message property
-          return error.error.errorMessage;
-        }
-
-        // Fallback to status text or a generic message
-        return error.statusText || 'An unexpected error occurred';
+    // Check if error response has a body with a message
+    if (error.error instanceof ErrorEvent) {
+      // Client-side error
+      return error.error.message || 'Client-side error occurred';
+    } else {
+      // Server-side error
+      // Try multiple ways to extract the error message
+      if (error.error && error.error.message) {
+        // Directly from error object
+        return error.error.message;
       }
-    }
 
+      if (error.error && typeof error.error === 'string') {
+        // If error is a string message
+        return error.error;
+      }
+
+      if (error.error && error.error.errorMessage) {
+        // Alternative error message property
+        return error.error.errorMessage;
+      }
+
+      // Fallback to status text or a generic message
+      return error.statusText || 'An unexpected error occurred';
+    }
+  }
 
   openReservationDialog() {
     this.reservationDialog.openDialog();
   }
 
-  isEoLogged(){
-    if(typeof window !== 'undefined' && window.localStorage){
-      return this.jwtService.getRoleFromToken()=='EO';
+  isEoLogged() {
+    if (typeof window !== 'undefined' && window.localStorage) {
+      return this.jwtService.getRoleFromToken() == 'EO';
     }
     return false;
   }
@@ -137,16 +161,15 @@ export class ServiceDetailsComponent implements OnInit {
   toggleFavorite(): void {
     this.isFavorited = !this.isFavorited;
     this.merchandiseService
-      .favorizeMerchandise(
-        this.service?.id,
-         this.jwtService.getIdFromToken()
-      )
+      .favorizeMerchandise(this.service?.id, this.jwtService.getIdFromToken())
       .pipe(tap((response) => {}))
       .subscribe();
   }
 
   calculatePrice(): number {
-    const discountedPrice = this.service!.price - (this.service!.price * this.service!.discount)/100;
+    const discountedPrice =
+      this.service!.price -
+      (this.service!.price * this.service!.discount) / 100;
     return Math.round(discountedPrice);
   }
 
@@ -157,6 +180,10 @@ export class ServiceDetailsComponent implements OnInit {
   }
 
   seeChat() {
-    this.router.navigate(['home', 'messenger',this.service?.serviceProviderId]);
+    this.router.navigate([
+      'home',
+      'messenger',
+      this.service?.serviceProviderId,
+    ]);
   }
 }
